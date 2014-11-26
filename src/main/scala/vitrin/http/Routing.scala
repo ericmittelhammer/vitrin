2f5d6f41830abf611ab041fcbe05a3a642adbaf8
Balázs Kéki <keki@gawker.com>
2014-11-26 17:25:07 +0100

    add http method support

--- src/main/scala/vitrin/http/Routing.scala
+++ src/main/scala/vitrin/http/Routing.scala
@@ -7,12 +7,19 @@ import vitrin._
 
 class Routing[F[_]](routes: (String, Regex => HttpRequest => F[HttpResponse])*) {
 
-	private case class Route(path: List[String], handler: Regex => HttpRequest => F[HttpResponse])
+	private case class Route(method: String, path: List[String], handler: Regex => HttpRequest => F[HttpResponse])
 
 	private case class Tree(nodes: Map[String, Tree], regex: String, handler: Option[Regex => HttpRequest => F[HttpResponse]]) {
 
 		def insert(route: Route): Tree = {
-			route.path match {
+			route.method match {
+				case "_" => insert("$" +: route.path, route.handler)
+				case _ => insert(route.method +: route.path, route.handler)
+			}
+		}
+
+		def insert(routePath: Seq[String], routeHandler: Regex => HttpRequest => F[HttpResponse]): Tree = {
+			routePath match {
 			  	case head :: tail =>
 					val regexPart = head match {
 						  case "$" => """(^/+)"""
@@ -21,56 +28,64 @@ class Routing[F[_]](routes: (String, Regex => HttpRequest => F[HttpResponse])*) 
 					}
 					nodes.get(head) match {
 						case Some(subtree) =>
-							Tree(nodes.updated(head, subtree.insert(Route(tail, route.handler))), regex, handler)
+							Tree(nodes.updated(head, subtree.insert(tail, routeHandler)), regex, handler)
 					  	case None =>
-							Tree(nodes + (head -> Tree(Map.empty, (regex + """\/""" + regexPart), None).insert(Route(tail, route.handler))), regex, handler)
+							Tree(nodes + (head -> Tree(Map.empty, (regex + """\/""" + regexPart), None).insert(tail, routeHandler)), regex, handler)
 					}
 			  	case Nil =>
-					Tree(nodes, regex, Some(route.handler))
+					Tree(nodes, regex, Some(routeHandler))
 			}
 		}
 
 		def matchRoute(path: List[String]): Option[HttpRequest => F[HttpResponse]] =
 			path match {
-			case Nil =>
-				handler.map(_(regex.r))
-			case head :: tail =>
-				nodes.get(head)
-					.orElse(nodes.get("#"))
-					.orElse(nodes.get("$"))
-					.flatMap(_.matchRoute(tail))
+				case Nil =>
+					handler.map(_(regex.r))
+				case head :: tail =>
+					nodes.get(head)
+						.orElse(nodes.get("#"))
+						.orElse(nodes.get("$"))
+						.flatMap(_.matchRoute(tail))
 			}
 
-	  override def toString: String = {
-		def toString0(path: String, node: Tree): Seq[String] = {
-		  val nodeList =
-			node.nodes flatMap {
-				case (l, n) =>
-					toString0(path + "/" + l, n)
-			}
-		  node.handler match {
-			case Some(h) =>
-			  path +: nodeList.toSeq
-			case None =>
-			  nodeList.toSeq
-		  }
+		override def toString: String = {
+			def toString0(path: String, node: Tree): Seq[String] = {
+				val nodeList =
+					node.nodes flatMap {
+						case (l, n) =>
+							toString0(path + "/" + l, n)
+					}
+				node.handler match {
+					case Some(h) =>
+						path +: nodeList.toSeq
+					case None =>
+						nodeList.toSeq
+				}
 		}
-		toString0("", this).mkString("\n")
-	  }
+			toString0("", this).mkString("\n")
+		}
 
 	}
 
 	private val parsedRoutes: Tree = {
+		val splitRoutes = routes map {
+			case (route, fn) =>
+				val (method, uri)  = parseRouteString(route)
+				Route(method, uri, fn)
+		}
 
-
-	  val splitRoutes = routes map {
-		case (path, fn) => Route(path.split('/').toList.drop(1), fn)
-	  }
-
-	  splitRoutes.foldLeft(Tree(Map.empty, "", None))(_ insert _)
+		splitRoutes.foldLeft(Tree(Map.empty, "", None))(_ insert _)
 	}
 
+	def parseRouteString(route: String): (String, List[String]) = {
+		val (method, uri) = route.splitAt(route.indexOf(' '))
+		(method, parseUri(uri))
+	}
+
+	def parseUri(uri: String): List[String] =
+		uri.dropWhile(_ == '/').split('/').toList
+
 	val run: PartialFunction[HttpRequest, F[HttpResponse]] = Function unlift { request =>
-		parsedRoutes.matchRoute(request.uri.path.toString.split('/').toList.drop(1)).map(_(request))
+		parsedRoutes.matchRoute(request.method.value +: parseUri(request.uri.path.toString)).map(_(request))
 	}
 }
