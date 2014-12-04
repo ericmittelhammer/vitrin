4a8842bbbc3cb88c76709db63b30d00ec02a1e1b
Mikolaj Szabo <savingprivateblue@gmail.com>
2014-12-04 23:29:22 +0100

    adjusting to new pattern of Server-Runtime-Environment
    Signed-off-by: Mikolaj Szabo <savingprivateblue@gmail.com>

--- src/main/scala/vitrin/runtime/MongoEnvironment.scala
+++ src/main/scala/vitrin/runtime/MongoEnvironment.scala
@@ -1,5 +1,7 @@
 package vitrin.runtime
 
+import vitrin.runtime.config.TypesafeConfig
+
 import akka.actor.ActorSystem
 
 import com.typesafe.config.{Config => TSConfig}
@@ -10,86 +12,79 @@ import reactivemongo.api.MongoConnectionOptions
 import reactivemongo.api.MongoDriver
 import reactivemongo.core.nodeset.Authenticate
 
-import vitrin.runtime.config.TypesafeConfig
-
-
 object MongoEnvironment {
-  case class ConfigOption(val v: String) extends AnyVal
-  case class Config(val v: TSConfig) extends AnyVal
- 
-  case class VitrinMongoConnection(connection: MongoConnection, dbName: Option[String])
+	case class ConfigOption(val v: String) extends AnyVal
+	case class Config(val v: TSConfig) extends AnyVal
+	case class VitrinMongoConnection(connection: MongoConnection, dbName: Option[String])
 }
 
 trait MongoEnvironment {
-  private val systemConfig = TypesafeConfig.akkaConfig("mongo-system")
-  private implicit val system = ActorSystem("mongo-system", systemConfig)
-  implicit val mongoConfig: MongoEnvironment.Config
-  
-   
-  private object MongoConnectionBuilder {
-    
-    import scala.collection.JavaConverters._
+	private val systemConfig = TypesafeConfig.akkaConfig("mongo-system")
+	private implicit val system = ActorSystem("mongo-system", systemConfig)
+	implicit val mongoConfig = MongoEnvironment.Config(TypesafeConfig().getConfig("mongo"))
 
-    implicit private def ConfigString(config: TSConfig, key: String): String = config.getString(key)
-    implicit private def ConfigInt(config: TSConfig, key: String): Int = config.getInt(key)
-    implicit private def ConfigBoolean(config: TSConfig, key: String): Boolean = config.getBoolean(key)
-    implicit private def ConfigStringList(config: TSConfig, key: String): List[String] = config.getStringList(key).asScala.toList
-    implicit private def ConfigConfigList(config: TSConfig, key: String): List[TSConfig] = config.getConfigList(key).asScala.toList
+ 	private object MongoConnectionBuilder {
+    	import scala.collection.JavaConverters._
 
-    private def getConfig[A](key: String)(implicit configGetter: (TSConfig, String) => A, config: MongoEnvironment.Config): Option[A] = {
-      if (config.v.hasPath(key)) {
-        Some(configGetter(config.v, key))
-      } else {
-        None
-      }
-    }
-    
-    private def getConfig[A](config: TSConfig, key: String)
-      (implicit configGetter: (TSConfig, String) => A): Option[A] = getConfig(key)(configGetter, MongoEnvironment.Config(config)) 
-    
-    def build(): MongoEnvironment.VitrinMongoConnection = {
-      val dbO: Option[String] = getConfig("db")
-      val userO: Option[String] = getConfig("user")
-      val passwordO: Option[String] = getConfig("password")
-      val connectTimeoutMS: Int = getConfig[Int]("connectTimeoutMS").getOrElse(0)
-      val authSource: Option[String] = getConfig("authSource")
-      val tcpNoDelay: Boolean = getConfig[Boolean]("tcpNoDelay").getOrElse(true)
-      val keepAlive: Boolean = getConfig[Boolean]("keepAlive").getOrElse(true)
-      val nbChannelsPerNode: Int = getConfig[Int]("nbChannelsPerNode").getOrElse(10)
-      
-      val hosts = getConfig[List[TSConfig]]("hosts").getOrElse(List()).flatMap { c =>
-        for {
-          host <- getConfig[String](c, "host")
-          port <- getConfig[Int](c, "port")
-        } yield (host, port)
-      }
-      
-      val ignoredOptions = getConfig[List[String]]("ignoredOptions").getOrElse(List())
+	    implicit private def ConfigString(config: TSConfig, key: String): String = config.getString(key)
+	    implicit private def ConfigInt(config: TSConfig, key: String): Int = config.getInt(key)
+	    implicit private def ConfigBoolean(config: TSConfig, key: String): Boolean = config.getBoolean(key)
+	    implicit private def ConfigStringList(config: TSConfig, key: String): List[String] = config.getStringList(key).asScala.toList
+	    implicit private def ConfigConfigList(config: TSConfig, key: String): List[TSConfig] = config.getConfigList(key).asScala.toList
 
-      val options = MongoConnectionOptions(
-          connectTimeoutMS = connectTimeoutMS,
-          authSource = authSource,
-          tcpNoDelay = tcpNoDelay,
-          keepAlive = keepAlive,
-          nbChannelsPerNode = nbChannelsPerNode)
-      
-      val authenticate = for {
-        db <- dbO
-        user <- userO
-        password <- passwordO
-      } yield Authenticate(user = user, password = password, db = db)
-      
-      val parameters = ParsedURI(
-          authenticate = authenticate,
-          db = dbO,
-          hosts = hosts,
-          ignoredOptions = ignoredOptions,
-          options = options)
-      val connection = (new MongoDriver).connection(parameters)
-      MongoEnvironment.VitrinMongoConnection(connection, dbO)
-    }
-  }
-  
-  lazy val mongo = MongoConnectionBuilder.build()
-  
+		private def getConfig[A](key: String)(implicit configGetter: (TSConfig, String) => A, config: MongoEnvironment.Config): Option[A] =
+			if (config.v.hasPath(key)) Some(configGetter(config.v, key))
+			else None
+
+		private def getConfig[A](config: TSConfig, key: String)(implicit configGetter: (TSConfig, String) => A): Option[A] =
+			getConfig(key)(configGetter, MongoEnvironment.Config(config))
+
+		def build(): MongoEnvironment.VitrinMongoConnection = {
+			val dbO: Option[String] = getConfig("db")
+			val userO: Option[String] = getConfig("user")
+			val passwordO: Option[String] = getConfig("password")
+			val connectTimeoutMS: Int = getConfig[Int]("connectTimeoutMS").getOrElse(0)
+			val authSource: Option[String] = getConfig("authSource")
+			val tcpNoDelay: Boolean = getConfig[Boolean]("tcpNoDelay").getOrElse(true)
+			val keepAlive: Boolean = getConfig[Boolean]("keepAlive").getOrElse(true)
+			val nbChannelsPerNode: Int = getConfig[Int]("nbChannelsPerNode").getOrElse(10)
+
+			val hosts = getConfig[List[TSConfig]]("hosts").getOrElse(List()).flatMap { c =>
+				for {
+					host <- getConfig[String](c, "host")
+					port <- getConfig[Int](c, "port")
+				} yield (host, port)
+			}
+
+			val ignoredOptions = getConfig[List[String]]("ignoredOptions").getOrElse(List())
+
+			val options = MongoConnectionOptions(
+				connectTimeoutMS = connectTimeoutMS,
+				authSource = authSource,
+				tcpNoDelay = tcpNoDelay,
+				keepAlive = keepAlive,
+				nbChannelsPerNode = nbChannelsPerNode
+			)
+
+			val authenticate = for {
+				db <- dbO
+				user <- userO
+				password <- passwordO
+			} yield Authenticate(user = user, password = password, db = db)
+
+			val parameters = ParsedURI(
+				authenticate = authenticate,
+				db = dbO,
+				hosts = hosts,
+				ignoredOptions = ignoredOptions,
+				options = options
+			)
+
+			val connection = (new MongoDriver).connection(parameters)
+
+			MongoEnvironment.VitrinMongoConnection(connection, dbO)
+		}
+	}
+
+	lazy val mongo = MongoConnectionBuilder.build()
 }
--- src/main/scala/vitrin/runtime/MongoRuntime.scala
+++ src/main/scala/vitrin/runtime/MongoRuntime.scala
@@ -1,36 +1,30 @@
 package vitrin.runtime
 
+import vitrin.runtime.logging.Logging
+
 import reactivemongo.api.Collection
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
-import vitrin.runtime.logging.Logging
-
-trait MongoRuntime extends DefaultRuntime {
-  self: Logging =>
-    
-  type Env <: DefaultEnvironment with MongoEnvironment
-  
-  def withMongo[A](dbName: String, collectionName: String)(fn: Collection => Future[A])(implicit ec: ExecutionContext) = {
-    fromFuture { env =>
-      val db = env.mongo.connection.db(dbName)
-      val collection = db(collectionName)
-      fn(collection)
-    }
-  }
-  
-  def withMongo[A](collectionName: String)(fn: Collection => Future[A])(implicit ec: ExecutionContext) = {
-    fromFuture { env =>
-      val dbName = env.mongo.dbName.getOrElse(throw new MongoRuntime.DatabaseMissingException("There is no default mongo database"))
-      val db = env.mongo.connection(dbName)
-      val collection = db(collectionName)
-      fn(collection)
-    }
-  }
-  
+object MongoRuntime {
+	class DatabaseMissingException(msg: String) extends Throwable(msg)
 }
 
-object MongoRuntime {
-  class DatabaseMissingException(msg: String) extends Throwable(msg)
+trait MongoRuntime[Env <: DefaultEnvironment with MongoEnvironment] extends DefaultRuntime[Env] {
+ 	self: Logging =>
+
+	def withMongo[A](dbName: String, collectionName: String)(fn: Collection => Future[A])(implicit ec: ExecutionContext) = fromFuture { env =>
+		val db = env.mongo.connection.db(dbName)
+		val collection = db(collectionName)
+		fn(collection)
+	}
+
+	def withMongo[A](collectionName: String)(fn: Collection => Future[A])(implicit ec: ExecutionContext) = fromFuture { env =>
+		val dbName = env.mongo.dbName.getOrElse(throw new MongoRuntime.DatabaseMissingException("There is no default mongo database"))
+		val db = env.mongo.connection(dbName)
+		val collection = db(collectionName)
+		fn(collection)
+	}
+
 }
